4289c8abd82b8b011bcf712d6f80444679e352d4
AB <baseerabdul203@gmail.com>
2025-05-09 15:47:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,7 @@ resource "aws_instance" "frontend" {
   ami           = "ami-0c15e602d3d6c6c4a"
   instance_type = "t2.micro"
   key_name      = "load"
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install epel -y
-              sudo yum update -y
-              sudo yum install python3 -y
-              EOF
+
   tags = {
     Name = "c8.local"
   }
