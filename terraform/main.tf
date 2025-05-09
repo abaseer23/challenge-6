aa8c94ff34c41e3c19909aed6594b00773aeb3ac
AB <baseerabdul203@gmail.com>
2025-05-09 11:47:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "frontend" {
   ami           = "ami-085386e29e44dacd7"
   instance_type = "t2.micro"
-  key_name      = "ubuntu"
+  key_name      = "load"
 
   tags = {
     Name = "c8.local"
@@ -28,7 +28,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami           = "ami-0f9de6e2d2f067fca"
   instance_type = "t2.micro"
-  key_name      = "ubuntu"
+  key_name      = "load"
 
   tags = {
     Name = "u21.local"
