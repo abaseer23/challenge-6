0fe62a877df2c890f07c54867cafe6f7460296c9
AB <baseerabdul203@gmail.com>
2025-05-09 14:57:21 +0530

    Update generate_inventory.sh

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -3,6 +3,9 @@
 FRONTEND_IP=$(terraform -chdir=../terraform output -raw frontend_ip)
 BACKEND_IP=$(terraform -chdir=../terraform output -raw backend_ip)
 
+[all:vars]
+ansible_python_interpreter=/usr/bin/python3
+
 cat <<EOF > inventory.ini
 [frontend]
 c8.local ansible_host=$FRONTEND_IP ansible_user=ec2-user
